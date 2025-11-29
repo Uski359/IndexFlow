819f4f0e67b1f25fc0ecf2455cf483eb2eea0e6a
Uski <uski539@gmail.com>
2025-11-30 00:37:38 +0300

    chore: remove config package sources

--- packages/config/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { loadBackendConfig } from './backend.js';
-export type { BackendConfig } from './backend.js';
-
-export { loadIndexNodeEnv } from './index-node.js';
-export type { IndexNodeEnv } from './index-node.js';
